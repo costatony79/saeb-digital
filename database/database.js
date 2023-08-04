16e0c75957aac580135efa15609ead38b0934099
Tony Santos Costa <33641618+costatony79@users.noreply.github.com>
2023-08-04 09:54:09 -0400

    Atualização database aluno aluno

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 //conexão com o banco de dados do mysql
-const connection = new Sequelize("saeb-digital-julho", "root", "admin", {
+const connection = new Sequelize("saeb-digital-julho", "aluno", "aluno", {
     host: "localhost",
     dialect: "mysql"
 });
